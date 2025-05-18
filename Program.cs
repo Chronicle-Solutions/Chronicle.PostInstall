4d57946a8483377b861ef7036ee3bc12066fdd40
Alex Dasneves <adasneves127@gmail.com>
2025-05-17 20:58:31 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Win32;
+
+namespace Chronicle.PostInstall
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Chronicle Post-Install");
+            Console.WriteLine("Setting Client ID");
+            Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\ChronicleSoftware", "clientID", Guid.NewGuid());
+
+            Console.Write("Authentication Server: ");
+            string authServer = Console.ReadLine() ?? "";
+            Registry.SetValue("HKEY_CURRENT_USER\\Software\\ChronicleSoftware", "AuthServer", authServer);
+
+            Console.WriteLine("Configuration Complete! Press any key to Exit.");
+            Console.ReadKey();
+
+        }
+    }
+}
